9123241a8c7d8f7f394ccc41e7059541ed7a25ae
taoziyu97 <baomihai@sina.com>
2020-08-27 20:10:03 +0800

    update cox TCGA

--- script/cox_analysis.R
+++ script/cox_analysis.R
@@ -20,7 +20,6 @@ load("C:/Users/lenovo/Documents/GitHub/PAWSASP/data/data/artefacts.Rdata")
 sig_name <- subset(sig_name, !(sig_name %in% c(artefacts)))
 
 ## 从做cox分析开始，把代码分成数据预处理和cox分析
-# (f <- as.formula(paste("Surv(OS.time, OS) ~ ", paste(sig_name, collapse = "+"))))
 
 TCGA_PAN_cox <- matrix(data = NA, nrow = 64, ncol = 7, 
                        dimnames = list(c(sig_name),
@@ -28,28 +27,6 @@ TCGA_PAN_cox <- matrix(data = NA, nrow = 64, ncol = 7,
                                          "HR","P value","CI upper", 
                                          "CI lower", "var"))) %>%
   as.data.frame()
-# TCGA_PAN_cox$`cancer type` <- "AML"
-# TCGA_PAN_cox$source <- "TCGA"
-# TCGA_PAN_cox$scale <- "WES"
-
-# TCGA_CT <- TCGA_cox_data$Cancer.Types[!duplicated(TCGA_cox_data$Cancer.Types)]
-
-# AML_cox_data <- TCGA_cox_data[which(TCGA_cox_data$Cancer.Types == "AML"), ]
-# 不同的cancer type可以不用写循环，而是按照cancer type来group变成list，lappy家族对每列进行处理
-# for (i in sig_name){
-#   f <- as.formula(paste("Surv(OS.time, OS) ~ ", paste(i)))
-#   res.cox <- coxph(f, data = AML_cox_data)
-#   summary_cox <- summary(res.cox)
-#   TCGA_PAN_cox[i, ] <- c("AML", "TCGA", "WES",
-#                        summary_cox$conf.int[1],
-#                        summary_cox$coefficients[5],
-#                        summary_cox$conf.int[3],
-#                        summary_cox$conf.int[4],
-#                        i) 
-#   TCGA_PAN_cox <- TCGA_PAN_cox %>% na.omit()
-# }
-
-
 
 # 针对所有癌症类型
 TCGA_sig_list <- TCGA_cox_data %>%
@@ -74,18 +51,9 @@ single_cox <- function(data){
 
 TCGA_sig_list$cox <- lapply(TCGA_sig_list$data, single_cox)
 
-# data = TCGA_sig_list$data[1]
-# function(data){
-#   for (i in sig_name){
-#     f <- as.formula(paste("Surv(OS.time, OS) ~ ", paste(i)))
-#     res.cox <- coxph(f, data = as.data.frame(data))
-#     summary_cox <- summary(res.cox)
-#     TCGA_PAN_cox[i,] <- c("cancer type", "TCGA", "WES",
-#                            summary_cox$conf.int[1],
-#                            summary_cox$coefficients[5],
-#                            summary_cox$conf.int[3],
-#                            summary_cox$conf.int[4],
-#                            i) 
-#     TCGA_PAN_cox <- TCGA_PAN_cox %>% na.omit()
-#   }
-# }
+
+# 多因素cox分析有一些问题，暂时先不考虑
+# (f <- as.formula(paste("Surv(OS.time, OS) ~ ", paste(test$var, collapse = "+"))))
+# test_data <- TCGA_sig_list$data[6] %>% as.data.frame()
+# res.cox_test <- coxph(Surv(OS.time, OS) ~ SBS1 + SBS2 + SBS3 + SBS5 + SBS9 + SBS10a + 
+#                         SBS10b + SBS13 + SBS15 + SBS17a , data = test_data)
